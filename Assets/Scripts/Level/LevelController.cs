d4d9230c9f12bb60364ee3080847cdb3ea3b8dbd
aubr1an <123403734+aubr1an@users.noreply.github.com>
2023-03-25 00:05:31 -0400

    Implemented Level Selection

--- Assets/Scripts/Level/LevelController.cs
+++ Assets/Scripts/Level/LevelController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+// avoid ambiguity
+using Random = UnityEngine.Random;
 
 public class LevelController : MonoBehaviour
 {
@@ -12,7 +15,8 @@ public class LevelController : MonoBehaviour
     public GameObject enemyPrefab4;
     public GameObject bossPrefab;
     public GameObject shieldPrefab;
-    int level=12;
+    //int level=12;
+    private int level;
     int enemiesRemaining;
     GameObject player;
     
@@ -22,6 +26,14 @@ public class LevelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (!PlayerPrefs.HasKey("SelectedLevel"))
+        {
+            PlayerPrefs.SetInt("SelectedLevel", 1);
+            PlayerPrefs.Save();
+        }
+
+        level = PlayerPrefs.GetInt("SelectedLevel");
+
         objectText.text="Level\n" + level.ToString();
         InitiateLevel();
         player=GameObject.Find("Player");
